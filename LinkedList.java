08d5cd0bbe5756d624e6b3e241a97d2c5d9e7082
Shahad <shahadalshehri05@gmail.com>
2026-04-20 18:55:10 +0300

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -1,14 +1,3 @@
-//
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-package cpcs204;
-
-/**
- *
- * @author Nojoud Alshehri
- */
 public class LinkedList {
 
     // head: reference variable to the first node of the list
